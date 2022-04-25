a785b56bfa456928404123be16ac8031bba74d29
rakshana.dharmaraj@thoughtworks.com <rakshana.dharmarajthoughtworks.com@Rakshanas-MacBook-Pro.local>
2022-04-25 20:49:15 +0530

    Trello#14 [Rakshana | Mohit] Add | Validation Tests for signup.

--- src/components/signUp/hooks/useSignup.spec.js
+++ src/components/signUp/hooks/useSignup.spec.js
@@ -4,6 +4,8 @@ import { createMemoryHistory } from 'history'
 import {when} from "jest-when";
 import useSignup from './useSignup'
 import signupFormService from '../services/signupFormService';
+import Signup from '../Signup';
+import {render} from "@testing-library/react";
 
 jest.mock("../services/signupFormService", () => ({
     __esModule: true,
@@ -30,7 +32,7 @@ describe("Signup evaluation", ()=> {
         phoneNumber: phoneNumber,
         password: password,
         confirmPassword: confirmPassword,
-    }
+    }    
 
     it("should initially not show error message", () => {
         const testOnSignUp = jest.fn();
@@ -56,4 +58,101 @@ describe("Signup evaluation", ()=> {
         expect(signupFormService.create).toHaveBeenCalledWith(signUpValues);
         expect(errorMessage()).toBe(undefined);
     });
+
+    it("should show error message when the name is invalid", async () => {
+
+    const newSignUpValues = {...signUpValues, name:"user1"}
+        const testOnSignUp = jest.fn();
+        const component = render(<Signup/>);
+        when(signupFormService.create).calledWith(newSignUpValues);
+        const history = createMemoryHistory();
+        const renderHookResult = renderHook(() => useSignup(testOnSignUp));
+        const result = renderHookResult.result;
+        const {handleSignup} = result.current;
+
+        await act(() => handleSignup(newSignUpValues, history));
+
+        const errorMessage = component.getByTestId("name").getAttribute("helperText")
+        expect(signupFormService.create).toBeCalledTimes(2);
+        expect(signupFormService.create).toHaveBeenCalledWith(newSignUpValues);
+        expect(errorMessage === "Name can only have letters.");
+    });
+
+    it("should show error message when the email is invalid", async () => {
+
+        const newSignUpValues = {...signUpValues, email:"useremail.com"}
+            const testOnSignUp = jest.fn();
+            const component = render(<Signup/>);
+            when(signupFormService.create).calledWith(newSignUpValues);
+            const history = createMemoryHistory();
+            const renderHookResult = renderHook(() => useSignup(testOnSignUp));
+            const result = renderHookResult.result;
+            const {handleSignup} = result.current;
+    
+            await act(() => handleSignup(newSignUpValues, history));
+    
+            const errorMessage = component.getByTestId("email").getAttribute("helperText")
+            expect(signupFormService.create).toBeCalledTimes(3);
+            expect(signupFormService.create).toHaveBeenCalledWith(newSignUpValues);
+            expect(errorMessage === "Email is invalid.");
+        });
+
+
+        it("should show error message when the password is invalid", async () => {
+
+            const newSignUpValues = {...signUpValues, password:"password"}
+                const testOnSignUp = jest.fn();
+                const component = render(<Signup/>);
+                when(signupFormService.create).calledWith(newSignUpValues);
+                const history = createMemoryHistory();
+                const renderHookResult = renderHook(() => useSignup(testOnSignUp));
+                const result = renderHookResult.result;
+                const {handleSignup} = result.current;
+        
+                await act(() => handleSignup(newSignUpValues, history));
+        
+                const errorMessage = component.getByTestId("password").getAttribute("helperText")
+                expect(signupFormService.create).toBeCalledTimes(4);
+                expect(signupFormService.create).toHaveBeenCalledWith(newSignUpValues);
+                expect(errorMessage === "Password is invalid.");
+        });
+
+        it("should show error message when the confirm password doesn't match password", async () => {
+
+            const newSignUpValues = {...signUpValues, password:"Password@1", confirmPassword:"Password@2"}
+                const testOnSignUp = jest.fn();
+                const component = render(<Signup/>);
+                when(signupFormService.create).calledWith(newSignUpValues);
+                const history = createMemoryHistory();
+                const renderHookResult = renderHook(() => useSignup(testOnSignUp));
+                const result = renderHookResult.result;
+                const {handleSignup} = result.current;
+        
+                await act(() => handleSignup(newSignUpValues, history));
+        
+                const errorMessage = component.getByTestId("confirmPassword").getAttribute("helperText")
+                expect(signupFormService.create).toBeCalledTimes(5);
+                expect(signupFormService.create).toHaveBeenCalledWith(newSignUpValues);
+                expect(errorMessage === "Confirm Password doesn't match password");
+        });
+
+        it("should show error message when the mobile number is invalid", async () => {
+
+            const newSignUpValues = {...signUpValues, phoneNumber:"0123456789"}
+                const testOnSignUp = jest.fn();
+                const component = render(<Signup/>);
+                when(signupFormService.create).calledWith(newSignUpValues);
+                const history = createMemoryHistory();
+                const renderHookResult = renderHook(() => useSignup(testOnSignUp));
+                const result = renderHookResult.result;
+                const {handleSignup} = result.current;
+        
+                await act(() => handleSignup(newSignUpValues, history));
+        
+                const errorMessage = component.getByTestId("mobileNumber").getAttribute("helperText")
+                expect(signupFormService.create).toBeCalledTimes(6);
+                expect(signupFormService.create).toHaveBeenCalledWith(newSignUpValues);
+                expect(errorMessage === "Mobile number is invalid");
+        });
+
 })
--- src/components/signUp/services/signupFormService.js
+++ src/components/signUp/services/signupFormService.js
@@ -17,15 +17,15 @@ const strongRegex = new RegExp(
 export const formSchema = object({
     name: string("Enter name")
         .required("Name is required")
-        .matches(/^[A-Za-z]+$/, "Name can only have letters."),
+        .matches(/^[A-Za-z .]+$/, "Name can only have letters."),
     username: string("Enter username").required("Username is required"),
     dob: string("Enter date of birth").required("Date of Birth is required"),
     email: string("Enter email")
         .required("Email is required")
-        .email("Enter a valid email"),
+        .email("Email is invalid."),
     phoneNumber: string("Enter mobile number")
         .required("Mobile Number is required")
-        .matches(/^[1-9][0-9]{9}$/, "Mobile number can only have 10 digits"),
+        .matches(/^[1-9][0-9]{9}$/, "Mobile number is invalid"),
     password: string("Enter password")
         .required("Password is required")
         .matches(strongRegex, "Password is invalid"),

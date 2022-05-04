a53d3ffd93f876f367d7553365ac111dfe4b9496
thejaswini.bk <thejaswini.bk@thoughtworks.com>
2022-05-04 14:14:03 +0530

    Trello#2 | [Thejaswini/Rakshana/Rhea] | Fix . added separate test suite for user profile feature disabling

--- src/components/user/UserProfile.spec.js
+++ src/components/user/UserProfile.spec.js
@@ -1,96 +1,92 @@
 import React from "react";
-import {fireEvent, render, screen} from "@testing-library/react";
-import {when} from "jest-when";
-import userService from './services/userService';
+import { fireEvent, render, screen } from "@testing-library/react";
+import { when } from "jest-when";
+import userService from "./services/userService";
 import ChangePasswordDialog from "./ChangePasswordDialog";
 import UserProfile from "./UserProfile";
-import '@testing-library/jest-dom/extend-expect';
+import "@testing-library/jest-dom/extend-expect";
 import useToggles from "../toggles/hooks/useToggles";
-import { FeatureToggleProvider} from "react-feature-toggles/lib";
-
+import { FeatureToggleProvider } from "react-feature-toggles/lib";
 
 jest.mock("./ChangePasswordDialog", () => {
-    return ({open}) => <div>Change Password is {open ? "open" : "closed"}</div>
+  return ({ open }) => <div>Change Password is {open ? "open" : "closed"}</div>;
 });
 
 jest.mock("../toggles/hooks/useToggles", () => ({
-    __esModule: true,
-    default: jest.fn()
+  __esModule: true,
+  default: jest.fn(),
 }));
 
 describe("Basic rendering and functionality", () => {
-    const openDialog = true;
-    const onClose = jest.fn();
-    const user = {username: "admin-user",
-                  name: null,
-                  dob: null,
-                  mobileNo: null,
-                  email: null,}
-
-
-    it("Should have Change password button when feature enabled", () => {
-        when(useToggles).calledWith().mockReturnValue({
-            toggleNames:{
-                CHANGE_PASSWORD: 'CHANGE_PASSWORD'
-               },      
-            toggles:{
-             CHANGE_PASSWORD : true,
-           }
-        });
-        const testToggles = {
-            CHANGE_PASSWORD: true
-            }
-            const {getByTestId} = render(
-            <FeatureToggleProvider featureToggleList={testToggles}>
-             <UserProfile />
-            </FeatureToggleProvider>);
-
-            expect(getByTestId('button-1')).toHaveTextContent('CHANGE PASSWORD');
+  const openDialog = true;
+  const onClose = jest.fn();
+  const user = {
+    username: "admin-user",
+    name: null,
+    dob: null,
+    mobileNo: null,
+    email: null,
+  };
+  when(useToggles)
+    .calledWith()
+    .mockReturnValue({
+      toggleNames: {
+        CHANGE_PASSWORD: "CHANGE_PASSWORD",
+      },
+      toggles: {
+        CHANGE_PASSWORD: true,
+      },
     });
+  const testToggles = {
+    CHANGE_PASSWORD: true,
+  };
 
-    it("Should display Change password dialog on click of button when feature enabled", () => {
-        when(useToggles).calledWith().mockReturnValue({
-            toggleNames:{
-                CHANGE_PASSWORD: 'CHANGE_PASSWORD'
-               },      
-            toggles:{
-             CHANGE_PASSWORD : true,
-           }
-        });
-        const testToggles = {
-            CHANGE_PASSWORD: true
-            }
+  it("Should have Change password button when feature enabled", () => {
+    const { getByTestId } = render(
+      <FeatureToggleProvider featureToggleList={testToggles}>
+        <UserProfile />
+      </FeatureToggleProvider>
+    );
 
-            const {getByText} = render(
-            <FeatureToggleProvider featureToggleList={testToggles}>
-                <UserProfile open={openDialog} onClose={onClose}/>
-            </FeatureToggleProvider>);       
+    expect(getByTestId("button-1")).toHaveTextContent("CHANGE PASSWORD");
+  });
 
-            expect(getByText("Change Password is closed")).toBeTruthy();
+  it("Should display Change password dialog on click of button when feature enabled", () => {
+    const { getByText } = render(
+      <FeatureToggleProvider featureToggleList={testToggles}>
+        <UserProfile open={openDialog} onClose={onClose} />
+      </FeatureToggleProvider>
+    );
 
-            fireEvent.click(getByText("CHANGE PASSWORD"));
+    expect(getByText("Change Password is closed")).toBeTruthy();
 
-            expect(getByText("Change Password is open")).toBeTruthy();
+    fireEvent.click(getByText("CHANGE PASSWORD"));
+
+    expect(getByText("Change Password is open")).toBeTruthy();
+  });
+});
+
+describe("should not display", () => {
+  when(useToggles)
+    .calledWith()
+    .mockReturnValue({
+      toggleNames: {
+        CHANGE_PASSWORD: "CHANGE_PASSWORD",
+      },
+      toggles: {
+        CHANGE_PASSWORD: false,
+      },
     });
+  const testToggles = {
+    CHANGE_PASSWORD: false,
+  };
+  it("Should not have Change password button when feature disabled", () => {
+    const { queryByText } = render(
+      <FeatureToggleProvider featureToggleList={testToggles}>
+        <UserProfile />
+      </FeatureToggleProvider>
+    );
 
-    it("Should not have Change password button when feature disabled", () => {
-        when(useToggles).calledWith().mockReturnValue({
-            toggleNames:{
-                CHANGE_PASSWORD: 'CHANGE_PASSWORD'
-               },      
-            toggles:{
-             CHANGE_PASSWORD : false,
-           }
-        });
-        const testToggles = {
-            CHANGE_PASSWORD: false
-            }
-            const {queryByText} = render(
-            <FeatureToggleProvider featureToggleList={testToggles}>
-             <UserProfile />
-            </FeatureToggleProvider>);
-
-            expect(queryByText("CHANGE PASSWORD")).toBeNull();
-    });
-
+    expect(queryByText("CHANGE PASSWORD")).toBeNull();
+  });
 });
